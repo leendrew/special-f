d4a341bb6e14caf2903550e4d12cc24a207dbe11
leendrew <leendrew99@gmail.com>
2024-02-27 00:33:06 +0600

    chore: style fixes; chore: using useRootColor

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRootColor } from './hooks';
+
+useRootColor();
+</script>
 
 <template>
   <RouterView />
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const cssVar = {
+  textColor: '--text-color',
+  bgColor: '--bg-color',
+} as const;
