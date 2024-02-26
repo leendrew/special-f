e1a5f44e3806475de3122cd0d3f69dad7c8139c6
leendrew <leendrew99@gmail.com>
2024-02-27 00:20:23 +0600

    feat: css var manipulation hook

---
+++ src/hooks/useRootColor.ts
@@ -0,0 +1,57 @@
+import { onMounted, onUnmounted } from 'vue';
+import { usePreferredDark } from '@vueuse/core';
+import * as colors from 'tailwindcss/colors';
+import { cssVar } from '@/constants';
+
+type ColorProps = {
+  text: string;
+  bg: string;
+};
+
+const defaultColors = {
+  light: {
+    text: colors.neutral[800],
+    bg: colors.neutral[200],
+  },
+  dark: {
+    text: colors.neutral[200],
+    bg: colors.neutral[800],
+  },
+};
+
+export function useRootColor(props?: ColorProps) {
+  const rootElement = document.documentElement;
+  const isDark = usePreferredDark();
+
+  const change = ({ text, bg }: ColorProps) => {
+    rootElement.style.setProperty(cssVar.textColor, text);
+    rootElement.style.setProperty(cssVar.bgColor, bg);
+  };
+
+  const reset = () => {
+    if (isDark.value) {
+      change({ text: defaultColors.dark.text, bg: defaultColors.dark.bg });
+      return;
+    }
+
+    change({ text: defaultColors.light.text, bg: defaultColors.light.bg });
+  };
+
+  onMounted(() => {
+    if (!props) {
+      if (isDark.value) {
+        change({ text: defaultColors.dark.text, bg: defaultColors.dark.bg });
+        return;
+      }
+
+      change({ text: defaultColors.light.text, bg: defaultColors.light.bg });
+      return;
+    }
+
+    change({ ...props });
+  });
+
+  onUnmounted(() => {
+    reset();
+  });
+}
