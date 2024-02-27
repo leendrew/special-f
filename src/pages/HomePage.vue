c23988335013eb37d4937bf48df48b688d9458dc
leendrew <leendrew99@gmail.com>
2024-02-28 01:16:09 +0600

    chore: pages refactor

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useRouter } from 'vue-router';
-import * as colors from 'tailwindcss/colors';
+import AppContainer from '@/components/ui/AppContainer.vue';
+import AppButton from '@/components/ui/AppButton.vue';
 import type { Data } from '@/components/sections/base.types';
 
 const router = useRouter();
 
 const data = {
   version: 1,
-  textColor: colors.zinc[800],
-  bgColor: colors.fuchsia[50],
+  textColor: '#27272a',
+  bgColor: '#fdf4ff',
 } satisfies Data;
 
 const hashedData = computed(() => btoa(JSON.stringify(data)));
@@ -20,13 +21,16 @@ const navigate = () => router.push({ name: 'hash', params: { hash: hashedData.va
 
 <template>
   <main class="main py-4">
-    <div class="container mx-auto my-0">
-      <div class="flex flex-col gap-2">
-        <h1>Home Page</h1>
-        <span>{{ hashedData }}</span>
-        <span>{{ data }}</span>
-        <button @click="navigate">To Page</button>
-      </div>
-    </div>
+    <AppContainer class="max-w-screen-lg">
+      <h1>Home Page</h1>
+      <p>{{ hashedData }}</p>
+      <p>{{ data }}</p>
+      <AppButton
+        class="mt-4"
+        @click="navigate"
+      >
+        To Page
+      </AppButton>
+    </AppContainer>
   </main>
 </template>
