ae698653c8d886b37df4f7f288fc20e11ba3776a
leendrew <leendrew99@gmail.com>
2024-02-28 01:15:41 +0600

    feat: ui

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { color, bgColor } = defineProps<{ color?: string; bgColor?: string }>();
+</script>
+
+<template>
+  <button
+    v-bind="$attrs"
+    class="block px-4 min-h-8 outline-offset-2 rounded-md duration-200 ease-out"
+    :style="{
+      color,
+      backgroundColor: bgColor,
+    }"
+    :class="{
+      'text-white': !color,
+      'bg-indigo-600 hover:bg-indigo-500': !bgColor,
+    }"
+  >
+    <slot />
+  </button>
+</template>
---
+++ src/components/ui/AppContainer.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto px-2 sm:px-4">
+    <slot />
+  </div>
+</template>
