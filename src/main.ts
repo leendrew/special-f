38e1bc6a9f7276ee023bd5b7bfcfbc2195438848
leendrew <leendrew99@gmail.com>
2024-02-27 23:43:43 +0600

    chore: using motion plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import { router } from '@/router';
+import { MotionPlugin } from '@vueuse/motion';
 import '@/main.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(MotionPlugin).mount('#app');
