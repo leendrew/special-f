5286e14a744636425239c09cbb655a7e90216934
leendrew <leendrew99@gmail.com>
2024-02-28 01:17:02 +0600

    feat: default type resolver

---
+++ src/components/sections/v1/V1Default.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import V1Base from './V1Base.vue';
+import type { V1SectionDefault } from './v1.types';
+import { calcTextDuration } from '@/utils';
+
+const { text, duration } = defineProps<V1SectionDefault>();
+const totalDuration = duration || calcTextDuration(text);
+</script>
+
+<template>
+  <V1Base
+    :text="text"
+    :duration="totalDuration"
+    :initial="{
+      y: -10,
+      opacity: 0,
+    }"
+    :enter="{
+      y: 0,
+      opacity: 1,
+      transition: {
+        bounce: 0,
+      },
+    }"
+  >
+    {{ text }}
+  </V1Base>
+</template>
