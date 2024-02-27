0f14c18e7ccd51a6cc0e0148bf8f884cf5b9abac
leendrew <leendrew99@gmail.com>
2024-02-28 01:16:39 +0600

    feat: base wrapper resolver

---
+++ src/components/sections/v1/V1Base.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const { text, duration, ...rest } = defineProps<{ text: string; duration: number; rest?: {} }>();
+const emit = defineEmits(['step:next', 'step:prev']);
+
+onMounted(() => {
+  setTimeout(() => {
+    emit('step:next');
+  }, duration);
+});
+</script>
+
+<template>
+  <div
+    v-motion="'v1Base'"
+    v-bind="rest"
+  >
+    <slot />
+  </div>
+</template>
