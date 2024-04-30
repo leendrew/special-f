d4d47323a59253895d32db8445b2ccbc1f8dda28
leendrew <leendrew99@gmail.com>
2024-04-30 09:58:25 +0600

    chore: corrects

--- src/pages/HashPage.vue
+++ src/pages/HashPage.vue
@@ -2,7 +2,7 @@
 import { useRoute, useRouter } from 'vue-router';
 import MainLayout from '@/components/ui/layouts/MainLayout.vue';
 import SectionsResolver from '@/components/sections/SectionsResolver.vue';
-import { encodeService } from '@/utils';
+import { decodeService } from '@/utils';
 import { ROUTE } from '@/constants';
 
 const {
@@ -11,7 +11,7 @@ const {
 const router = useRouter();
 
 const [version, welcomeData, sectionsData] = JSON.parse(
-  encodeService.base64ToString(hash as string),
+  decodeService.base64ToString(hash as string),
 );
 
 if (typeof version !== 'number') {
--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -1,24 +1,24 @@
 export const encodeService = {
-  stringToBase64(string: string) {
+  stringToBase64(string: string): string {
     return btoa(unescape(encodeURIComponent(string)));
-  },
-  base64ToString(base64String: string) {
-    return decodeURIComponent(escape(atob(base64String)));
   },
 };
 
 export const decodeService = {
-  arrayBufferToUtf8String(arrayBuffer: ArrayBuffer) {
+  base64ToString(base64String: string): string {
+    return decodeURIComponent(escape(atob(base64String)));
+  },
+  arrayBufferToUtf8String(arrayBuffer: ArrayBuffer): string {
     return new TextDecoder().decode(arrayBuffer);
   },
-  arrayBufferToBase64String(arrayBuffer: ArrayBuffer) {
+  arrayBufferToBase64String(arrayBuffer: ArrayBuffer): string {
     return btoa(
       Array.from(new Uint8Array(arrayBuffer))
         .map((byte) => String.fromCharCode(byte))
         .join(''),
     );
   },
-  arrayBufferToHexString(arrayBuffer: ArrayBuffer) {
+  arrayBufferToHexString(arrayBuffer: ArrayBuffer): string {
     return Array.from(new Uint8Array(arrayBuffer))
       .map((byte) => byte.toString(16).padStart(2, '0'))
       .join('');
