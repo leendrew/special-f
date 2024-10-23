7f69c0e14ef839fa901a50e8a052275424902ab5
leendrew <leendrew99@gmail.com>
2024-10-23 23:15:29 +0600

    feat: add app modal to global import

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { MotionPlugin } from '@vueuse/motion';
 import AppContainer from '@/components/ui/AppContainer.vue';
 import AppButton from '@/components/ui/AppButton.vue';
 import AppOverlay from '@/components/ui/AppOverlay.vue';
+import AppModal from '@/components/ui/AppModal.vue';
 import '@/main.css';
 
 createApp(App)
   .component('AppContainer', AppContainer)
   .component('AppButton', AppButton)
   .component('AppOverlay', AppOverlay)
+  .component('AppModal', AppModal)
   .use(router)
   .use(MotionPlugin)
   .mount('#app');
