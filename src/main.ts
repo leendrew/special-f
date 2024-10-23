b80cc059247bd46d69774b38279728a8de4e8dc4
leendrew <leendrew99@gmail.com>
2024-10-23 23:10:58 +0600

    feat: add app overlay to global import

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { router } from '@/router';
 import { MotionPlugin } from '@vueuse/motion';
 import AppContainer from '@/components/ui/AppContainer.vue';
 import AppButton from '@/components/ui/AppButton.vue';
+import AppOverlay from '@/components/ui/AppOverlay.vue';
 import '@/main.css';
 
 createApp(App)
   .component('AppContainer', AppContainer)
   .component('AppButton', AppButton)
+  .component('AppOverlay', AppOverlay)
   .use(router)
   .use(MotionPlugin)
   .mount('#app');
