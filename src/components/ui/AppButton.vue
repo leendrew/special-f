62f6ab0c4110ffaf486bc7dda2ef1c4bbeb9f51f
leendrew <leendrew99@gmail.com>
2024-10-23 23:29:01 +0600

    chore: refactor

--- src/components/ui/AppButton.vue
+++ src/components/ui/AppButton.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { useAttrs } from 'vue';
+
 const { color, bgColor } = defineProps<{ color?: string; bgColor?: string }>();
+const attrs = useAttrs();
+
+const isDisabled = attrs.disabled;
 </script>
 
 <template>
   <button
-    v-bind="$attrs"
-    class="block px-4 min-h-8 text-base outline-offset-2 rounded-md duration-200 ease-out"
+    v-bind="attrs"
+    class="block px-4 min-h-8 text-base outline-offset-2 rounded-md duration-200 ease-out cursor-pointer"
     :style="{
       color,
       backgroundColor: bgColor,
@@ -13,6 +18,7 @@ const { color, bgColor } = defineProps<{ color?: string; bgColor?: string }>();
     :class="{
       'text-white': !color,
       'bg-indigo-600 hover:bg-indigo-500': !bgColor,
+      'opacity-50': isDisabled,
     }"
   >
     <slot />
